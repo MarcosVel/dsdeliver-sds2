82e254b5c13f3436bccb4c702d995c4a195666e9
MarcosVel <marcos.veloso@sempreceub.com>
2021-01-06 23:34:16 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,25 @@
+import './styles.css';
+import { ReactComponent as YoutubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstaIcon } from './instagram.svg';
+
+function Footer() {
+  return (
+    <footer className="main-footer">
+      <p>App desenvolvido durante a 2ª ed. do evento Semana DevSuperior</p>
+      <div className="footer-icons">
+        <a href='https://www.youtube.com/DevSuperior' target='_new'>
+          <YoutubeIcon />
+        </a>
+        <a href='https://www.linkedin.com/in/marcosveloso99/' target='_new'>
+          <LinkedinIcon />
+        </a>
+        <a href='https://www.instagram.com/marcos_veloso21/' target='_new'>
+          <InstaIcon />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
